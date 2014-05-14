de48bf7356788dd5098e4a8acf0bb22872595079
marco-lindsay <lindsay_marco@hotmail.com>
2014-05-14 16:10:13 -0700

    last bunny

--- bunny.rb
+++ bunny.rb
@@ -1,14 +1,29 @@
 require 'rrobots'
 
-class FluffyBunny
+class Bunny
   include Robot
 
   def tick events
-    # battlefield_height = 100
     turn_radar 1 if time == 0
     turn_gun 10
-    accelerate 8
-    turn 3
+    accelerate 0.5
+    turn 2
     fire 3 unless events['robot_scanned'].empty?
   end
+  def health_low
+    if energy <= 90
+    accelerate 1
+    turn 3
+    else
+    turn_radar 1 if time == 0
+    turn_gun 10
+    accelerate 0.5
+    turn 2
+    fire 3 unless events['robot_scanned'].empty?
+    end
+  end
+
 end
+
+
+# marco and eric
